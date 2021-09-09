1170c608d6ced171401a16f3de039104a1da5ce2
dalejbarr <dalejbarr@protonmail.com>
2021-09-09 08:43:28 +0100

    remove .onLoad

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  if (base::requireNamespace("webex", quietly = TRUE)) {
-    warning("You have an outdated version of this package installed ('webex').\n",
-            "To avoid problems, please uninstall the old version using:\n",
-            "  remove.packages(\"webex\")")
-  }
-}
